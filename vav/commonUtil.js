2446ccedda9884c37223f87ba7fac36aebb4e509
Vinay Kumar V C <84114504+VINUVARDHAN@users.noreply.github.com>
2024-06-01 23:41:25 +0530

    Update commonUtil.js

--- vav/commonUtil.js
+++ vav/commonUtil.js
@@ -1,8 +1,9 @@
 const utils = require('../utils');
 
-const queriesHandling = {
+
+queriesHandling = {
     getQueryResult: async function (psql, query, values) {
-        let result = {};
+        result = {};
         try {
             const res = await psql.query(query, values);
             return { isSuccess: true, result: res.rows };
@@ -10,49 +11,48 @@ const queriesHandling = {
             return { isSuccess: false, result: err };
         }
     }
-};
+}
 
-const commonUtil = {
-    responseConstruction: {
-        userLoginSuccess: function (res) {
-            res.status(200).json({
-                success: true,
-                message: 'User login successful',
-                baseURL: utils.baseURL
-            });
-        },
-        userLoginUnsuccess: function (res) {
-            res.status(404).json({
-                success: false,
-                message: 'No user with these details'
-            });
-        },
-        ISE: function (res) {
-            res.status(500).json({
-                success: false,
-                message: 'Internal Server Error'
-            });
-        }
+
+commonUtil = {}
+commonUtil.responseConstruction = {
+    userLoginSuccess: function (res) {
+        res.status(200).json({
+            success: true,
+            message: 'User login successfull',
+            baseURL: utils.baseURL
+        });
     },
-    userUtils: {
-        isValidUser: async function (req, res, psql) {
-            const emailId = utils.trimString(req.body.emailId);
-            const password = utils.trimString(req.body.password);
-            const query = 'SELECT * FROM vavUserDetails WHERE emailId = $1 AND password = $2';
-            const values = [emailId, password];
-            const result = await queriesHandling.getQueryResult(psql, query, values);
-            if (result.isSuccess) {
-                if (result.result.length > 0) {
-                    req.vavUserDetails = result.result[0];
-                    commonUtil.responseConstruction.userLoginSuccess(res);
-                } else {
-                    commonUtil.responseConstruction.userLoginUnsuccess(res);
-                }
-            } else {
-                commonUtil.responseConstruction.ISE(res);
-            }
+    userLoginUnsuccess: function (res) {
+        res.status(404).json({
+            success: false,
+            message: 'No user with is details'
+        });
+    },
+    ISE: function (res) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+    }
+}
+commonUtil.userUtils = {
+    isValidUser: async function (req,res, psql) {
+        emailId = utils.trimString(req.body.emailId);
+        password = utils.trimString(req.body.password);
+        var query = 'SELECT * FROM vavUserDetails WHERE emailId = $1 and password = $2';
+        var values = [emailId, password];
+        var result = await queriesHandling.getQueryResult(psql, query, values);
+        if (result.isSuccess) {
+            req.vavUserDetails = result.result[0];
+            commonUtil.responseConstruction.userLoginSuccess(res);
+        } else {
+            commonUtil.responseConstruction.userLoginUnsuccess(res);
         }
     }
-};
+}
+
+
+
 
 module.exports = commonUtil;
